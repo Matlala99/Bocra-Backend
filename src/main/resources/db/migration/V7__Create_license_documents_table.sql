9a6be2a450642b51cfacaa1bee96040813d40ea1
AW3 CODES <168534423+Matlala99@users.noreply.github.com>
2026-03-27 14:36:01 +0200

    Update V7__Create_license_documents_table.sql

--- src/main/resources/db/migration/V7__Create_license_documents_table.sql
+++ src/main/resources/db/migration/V7__Create_license_documents_table.sql
@@ -1,1 +1,6 @@
 
+CREATE TABLE IF NOT EXISTS `license_documents` (
+  `license_id` BIGINT NOT NULL,
+  `document_url` VARCHAR(255) NOT NULL,
+  CONSTRAINT `fk_license_documents_license_id` FOREIGN KEY (`license_id`) REFERENCES `license_applications` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
