a8a9ae6e031f0bb8d9099a8ab207a564e4bd7a1c
AW3 CODES <168534423+Matlala99@users.noreply.github.com>
2026-03-27 14:34:00 +0200

    Update V2__Create_complaints_table.sql

--- src/main/resources/db/migration/V2__Create_complaints_table.sql
+++ src/main/resources/db/migration/V2__Create_complaints_table.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(255) NOT NULL UNIQUE,
+  `password` VARCHAR(255) NOT NULL,
+  `role` VARCHAR(50),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
